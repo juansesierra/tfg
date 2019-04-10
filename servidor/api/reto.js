d4cc62a4af4d21f3ccd29120ef0973fd7d1442d6
juansesierra <juansesierra5@gmail.com>
2019-04-10 12:08:00 +0200

    Procesar retos con promesas

--- servidor/api/reto.js
+++ servidor/api/reto.js
@@ -57,128 +57,139 @@ function listarRetos(callback) {
     })
 }
 
-function findByNombre(nombre, callback) {
-    
-    if (nombre) {
-        knex.select().from('reto').where("nombre", nombre)
-        .then(function(datos){
-            if (datos.length<1) {
-                callback({err:404});
-            }
-            else {
-                callback({data:datos[0]});
-            }
+function findByNombre(nombre) {
 
-        })
-    }
-    else {
-        callback({err:404});
-    }
-    
+    return new Promise((resolve, reject)=>{
+        if (nombre) {
+            knex.select().from('reto').where("nombre", nombre)
+            .then(function(datos){
+                if (datos.length<1) {
+                    reject({err:404});
+                }
+                else {
+                    resolve({data:datos[0]});
+                }
+
+            })
+        }
+        else {
+            reject({err:404});
+        }
+    });
+
 }
 
 app.post('/retos', function (req, resp) {
-    let nueva = req.body;
+    let nuevo = req.body;
     var responseObj = {};
+    var reto;
 
     if (req.files) {
         
     }
     
     try {
-        addReto(nueva, function(reto){
-			if(reto.err){
-                resp.status(reto.err)
-				resp.send({error: reto.message})                
-			}else{
-                subirArchivos(req, resp, solucion =>  {
-                    console.log(solucion)
-                    addSolucion(reto.data, solucion, response => {
-                       
-                        if (response.err) {
-                            resp.status(reto.err)
-				            resp.send({error: reto.message})
-                        }
-                        else {
-                            resp.status(201); // reto insertado               
-                            responseObj.data = "reto insertado con éxito!";
-                            resp.send(responseObj)
-                        }
-                    })
-                });
-			}
-		})
+
+        addReto(nuevo)
+        .then( response => {
+            reto = response;
+            return subirArchivos(req, resp);
+        })
+        .then (solucion => {
+            console.log(solucion);
+            return addSolucion(reto.data, solucion)
+        })
+        .then (response => {
+            resp.status(201); // reto insertado               
+            responseObj.data = "reto insertado con éxito!";
+            resp.send(responseObj)
+        })
+        .catch(error => {
+            if (error.err) {
+                resp.status(error.err)
+            }
+            else {
+                resp.status(500)
+            }
+            
+            resp.send({error: error.message})
+        })
+
     } catch(err) {
         resp.status(500)
 		resp.send({error:err.message})
     }
 })
 
-function subirArchivos (req, resp, callback) {
+function subirArchivos (req, resp) {
     var solucion = {};
 
-    //console.log(req);
-    exec ("mkdir ./retos/" + req.body.nombre, function (error) {
-        if (error == null) {
-            solucion.entrada = "./retos/" + req.body.nombre + "/" + req.files.entrada.name;
-            solucion.salida = "./retos/" + req.body.nombre + "/" + req.files.salida.name;
-    
-            let File = req.files.entrada;
-    
-            // Subimos la entrada al servidor
-            File.mv(solucion.entrada).then( function () {
+    return new Promise((resolve, reject)=>{
+        //console.log(req);
+        exec ("mkdir ./retos/" + req.body.nombre, function (error) {
+            if (error == null) {
+                solucion.entrada = "./retos/" + req.body.nombre + "/" + req.files.entrada.name;
+                solucion.salida = "./retos/" + req.body.nombre + "/" + req.files.salida.name;
+        
+                let File = req.files.entrada;
+        
+                // Subimos la entrada al servidor
+                File.mv(solucion.entrada).then( function () {
 
-                return File.mv(solucion.salida);
+                    return File.mv(solucion.salida);
 
-            }).then(function () {
-            
-                callback(solucion);
+                }).then(function () {
+                
+                    resolve(solucion);
 
-            }).catch(function (err){
+                }).catch(function (err){
+                    resp.status(500)
+                    resp.send({error: "Error al subir los ficheros de entrada/salida"});
+                })
+            }
+            else {
+                console.log("error al crear directorio")
                 resp.status(500)
-                resp.send({error: "Error al subir los ficheros de entrada/salida"});
-            })
-        }
-        else {
-            resp.status(500)
-            resp.send({error:"Error al crear el directorio del reto"});
-        }
-    });
+                resp.send({error:"Error al crear el directorio del reto"});
+            }
+        });
+    })
 }
 
-function addSolucion (reto, solucion, callback) {
+function addSolucion (reto, solucion) {
     
-    findByNombre(reto.nombre, function(nuevo) {
-        if(nuevo.err) {
-            callback({err:nuevo.err});
-        }
-        //Si no existe lo insertamos 
-        else {
-            knex('solucion_reto').insert({
+    return new Promise((resolve, reject)=>{
+        findByNombre(reto.nombre).then( function(nuevo) {
+            
+            return knex('solucion_reto').insert({
                 reto: reto.id,
                 entrada: solucion.entrada,
                 salida: solucion.salida
             })
             .then(function(insertado) {
                 if (insertado.length<1) {
-                    callback({err:500});            
+                    reject({err:500});            
                 }
                 else {
-                    callback({data:insertado});
+                    resolve({data:insertado});
                 }
             }) 
-        }
+            
+        }). catch(error => {
+            reject({err:error});
+        })
     })
 }
 
-function addReto (reto, callback) {
-    // buscamos si existe el reto a insertar
-    findByNombre(reto.nombre, function(nuevo) {
-        if(nuevo.data) {
-            callback({err:403, message:"Ya existe un reto con este nombre"});
-        }
-        //Si no existe lo insertamos 
-        else {
+function addReto (reto) {
+
+    return new Promise((resolve, reject)=>{
+        // buscamos si existe el reto a insertar
+        findByNombre(reto.nombre).then( function(nuevo) {
+            if(nuevo.data) {
+                reject({err:403, message:"Ya existe un reto con este nombre"});
+            }
+        }).catch (error => {
             if (reto.descripcion) {
                 knex('reto').insert({
                     nombre: reto.nombre,
@@ -186,22 +197,21 @@ function addReto (reto, callback) {
                 })
                 .then(function(insertado) {
                     if (insertado.length<1) {
-                        callback({err:500});            
+                        reject({err:500});            
                     }
                     else {
                         reto.id = insertado[0];
-                        callback({data:reto});
+                        resolve({data:reto});
                     }
                 }) 
             } 
             else {
-                callback({
+                reject({
                     err: 400,
                     message: "Faltan parametros"
                 })
             }
-        }
-    })  
-             
+        })  
+    })         
         
 }
