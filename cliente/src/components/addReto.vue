10f06ce584c936d5a9a626eb7bb07cf4e0e29f17
juansesierra <juansesierra5@gmail.com>
2019-07-24 16:36:26 +0200

    creacion de navbar

--- cliente/src/components/addReto.vue
+++ cliente/src/components/addReto.vue
@@ -1,52 +1,55 @@
 <template>
-    <div id="addReto_container"> 
-        <h1> Nuevo Reto </h1>
-        <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-        <b-form-group id="input-group-1" label="Nombre:" label-for="input-1" >
-            <b-form-input
-                id="input-1"
-                v-model="form.nombre"
-                required
-                placeholder="Introduce el nombre del reto"
-            ></b-form-input>
-        </b-form-group>
-
-        <b-form-group id="input-group-2" label="Descripcion:" label-for="input-2">
-            <b-form-textarea
-                id="input-2"
-                v-model="form.descripcion"
-                required
-                placeholder="Introduce la  descripcion del reto"
-                rows="3"
-                max-rows="6"
-            ></b-form-textarea>
-        </b-form-group>
-
-        <div v-for="prueba in form.pruebas" :key="prueba.id">
-
-            <b-form-group id="input-group-3" label="Fichero de entrada:" label-for="input-entrada">
-                <b-form-file
-                    id="input-entrada"
-                    v-model="prueba.f_entrada"
+    <div>
+        <navbar></navbar>
+        <div id="addReto_container"> 
+            <h1> Nuevo Reto </h1>
+            <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+            <b-form-group id="input-group-1" label="Nombre:" label-for="input-1" >
+                <b-form-input
+                    id="input-1"
+                    v-model="form.nombre"
                     required
-                    placeholder="Selecciona el fichero de entrada"
-                ></b-form-file>
+                    placeholder="Introduce el nombre del reto"
+                ></b-form-input>
             </b-form-group>
 
-            <b-form-group id="input-group-3" label="Fichero de salida:" label-for="input-salida">
-                <b-form-file
-                    id="input-salida"
-                    v-model="prueba.f_salida"
+            <b-form-group id="input-group-2" label="Descripcion:" label-for="input-2">
+                <b-form-textarea
+                    id="input-2"
+                    v-model="form.descripcion"
                     required
-                    placeholder="Selecciona el fichero de salida"
-                ></b-form-file>
+                    placeholder="Introduce la  descripcion del reto"
+                    rows="3"
+                    max-rows="6"
+                ></b-form-textarea>
             </b-form-group>
+
+            <div v-for="prueba in form.pruebas" :key="prueba.id">
+
+                <b-form-group id="input-group-3" label="Fichero de entrada:" label-for="input-entrada">
+                    <b-form-file
+                        id="input-entrada"
+                        v-model="prueba.f_entrada"
+                        required
+                        placeholder="Selecciona el fichero de entrada"
+                    ></b-form-file>
+                </b-form-group>
+
+                <b-form-group id="input-group-3" label="Fichero de salida:" label-for="input-salida">
+                    <b-form-file
+                        id="input-salida"
+                        v-model="prueba.f_salida"
+                        required
+                        placeholder="Selecciona el fichero de salida"
+                    ></b-form-file>
+                </b-form-group>
+            </div>
+
+            <b-button type="submit" variant="primary">Añadir</b-button>
+            <b-button type="submit" @click.prevent="addPrueba" variant="info">Añadir entrada/salida</b-button>
+            <b-button type="reset" variant="danger">Reiniciar</b-button>
+            </b-form>
         </div>
-
-        <b-button type="submit" variant="primary">Añadir</b-button>
-        <b-button type="submit" @click.prevent="addPrueba" variant="info">Añadir entrada/salida</b-button>
-        <b-button type="reset" variant="danger">Reiniciar</b-button>
-        </b-form>
     </div>
     
 </template>
@@ -54,24 +57,28 @@
 
 <script>
     import {Servicio_API} from './../API.js';
+    import Navbar from './navbar.vue';
 
 
     var servicio_API = new Servicio_API();
 
     export default {
+        components: {
+            Navbar
+        },
         data() {
-        return {
-            form: {
-            nombre: '',
-            descripcion: '',
-            pruebas: [{
-                id: 0,
-                f_entrada: null,
-                f_salida: null
-            }]
-            },
-            show: true
-        }
+            return {
+                form: {
+                nombre: '',
+                descripcion: '',
+                pruebas: [{
+                    id: 0,
+                    f_entrada: null,
+                    f_salida: null
+                }]
+                },
+                show: true
+            }
         },
         methods: {
         onSubmit(evt) {
